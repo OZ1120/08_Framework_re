caad19e993979501f3adcb73985461e8ea53acf5
OZ <macoto8@naver.com>
2024-04-08 20:32:04 +0900

    240408_ Spring boot 프로젝트 생성 및 설정

---
+++ practiceProject/src/main/java/com/lej/practice/PracticeProjectApplication.java
@@ -0,0 +1,14 @@
+package com.lej.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProjectApplication {
+
+	// 메인메서드 실행
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProjectApplication.class, args);
+	}
+
+}
